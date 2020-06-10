6276733ba728f3e44da1bf2086cc5a4bb9d2c575
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-06-10 15:13:12 -0500

    Crear tabla peewee

--- main.py
+++ main.py
@@ -1,68 +1,25 @@
-import pymysql as MySQLdb
+import peewee
+import datetime
 
 HOST = 'localhost'
 USER = 'root'
 PASSWORD = ''
 DATABASE = 'minicurso_python'
 
-USER_TABLE = """CREATE TABLE users(
-                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                username VARCHAR(50) NOT NULL,
-                password VARCHAR(50) NOT NULL
-                )"""
-DROP_TABLE = "DROP TABLE IF EXISTS `users` "      
-SHOW_TABLES = "SHOW TABLES"
-INSERT_USER = "INSERT INTO users (username,password) VALUES ( '{username}', '{password}' )"
-SELECT_USER = "SELECT * FROM users WHERE id = {id}"
-UPDATE_USER = "UPDATE users SET username ='{username}', password='{password}' WHERE id = {id}"
-DELETE_USER = "DELETE FROM users WHERE id = {id}"
+database = peewee.MySQLDatabase(DATABASE,host=HOST,port=3306,user=USER,password=PASSWORD)
 
-def execute_query(query):
-    try:
-        cursor.execute(query)
-        connection.commit()
-    except:
-        connection.rollback()
+class User(peewee.Model):
+    username = peewee.CharField(unique=True, max_length=50, index=True)
+    password = peewee.CharField(max_length=50)
+    email = peewee.CharField(max_length=50, null=True)
+    active = peewee.BooleanField(default=True)
+    created_date = peewee.DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = database
+        db_table = 'users'
 
 if __name__ == '__main__':
-    try:
-        connection = MySQLdb.connect(HOST,USER, PASSWORD, DATABASE)
+    if User.table_exists() is False:
+        User.create_table()    
 
-        cursor = connection.cursor()
-        cursor.execute(DROP_TABLE)
-        cursor.execute(USER_TABLE)
-
-        username = input("Ingrese el username")
-        password = input("Ingrese el password")
-
-        query = INSERT_USER.format(username=username, password=password)
-        print(query)
-        execute_query(query)        
-
-        query = UPDATE_USER.format(username='nuevo', password='nuevo', id=1)
-        print(query)
-        execute_query(query)
-
-        query = SELECT_USER.format(id=1)        
-
-        cursor.execute(query)
-        users = cursor.fetchall()
-
-        for user in users:
-            print(user)
-
-        query = DELETE_USER.format(id=1)
-        print(query)
-        execute_query(query)
-
-        """
-        cursor.execute(SHOW_TABLES)
-        tables = cursor.fetchall()
-
-        for table in tables:
-            print(table)
-        """
-
-        connection.close()
-    except MySQLdb.Error as error:
-        print(error)
