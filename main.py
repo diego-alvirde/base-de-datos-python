5ff8668e5702cdb3b5be2f4ffe3625ed2d708ac3
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-06-10 14:14:12 -0500

    Crear conexión

---
+++ main.py
@@ -0,0 +1,16 @@
+import pymysql as MySQLdb
+
+HOST = 'localhost'
+USER = 'root'
+PASSWORD = ''
+DATABASE = 'minicurso_python'
+
+if __name__ == '__main__':
+    try:
+        connection = MySQLdb.connect(HOST,USER, PASSWORD, DATABASE)
+
+        cursor = connection.cursor()
+
+        connection.close()
+    except MySQLdb.Error as error:
+        print(error)
