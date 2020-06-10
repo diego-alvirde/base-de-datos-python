df5381a3d1fb36a0c9b981042cd7a9a6e6011542
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-06-10 17:07:43 -0500

    Insertar datos peewee

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ USER = 'root'
 PASSWORD = ''
 DATABASE = 'minicurso_python'
 
-database = peewee.MySQLDatabase(DATABASE,host=HOST,port=3306,user=USER,password=PASSWORD)
+database = peewee.MySQLDatabase(DATABASE, host=HOST, port=3306, user=USER, password=PASSWORD)
+
 
 class User(peewee.Model):
     username = peewee.CharField(unique=True, max_length=50, index=True)
@@ -19,7 +20,32 @@ class User(peewee.Model):
         database = database
         db_table = 'users'
 
+
 if __name__ == '__main__':
     if User.table_exists() is False:
-        User.create_table()    
+        User.create_table()
 
+    # 1
+    """
+    user = User()
+    user.username = 'Diego'
+    user.password = 'password'
+    user.email = 'dalvirdem@mail.com'
+    user.save()
+    """
+    # 2
+    """
+    user = User(username='Josue', password='123456', email='jalvirdem@mail.com')
+    user.save()
+    """
+    # 3
+    """
+    user = {'username': 'Eder', 'password': 'eder'}
+    user = User(**user)
+    user.save()
+    """
+    # 4
+    #user = User.create(username='Armando', password='123456', email='armando@mail.com')
+    # 5
+    query = User.insert(username='Alondra', password='123456')
+    query.execute()
